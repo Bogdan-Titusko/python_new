0ef289000b739fe06e7496f932bb4e9f45d4b5f6
Bogdan Titusko <Titushko.bogdan@gmail.com>
2022-08-02 20:45:42 +0300

    Test (1 - 5)

---
+++ test1/test.py
@@ -0,0 +1,27 @@
+
+print('Hi please enter your name ')
+user_name = input("Name : ")
+last_name = input("Last name : ")
+age = input("Age : ")
+print( 'Hello ' + last_name + ' ' + user_name + '.' + 'Your age is '  + age)
+
+
+
+a = 3
+b = 4
+c = None
+
+
+hypotenuse = c = math.sqrt(a * a + b * b)
+
+print(int(hypotenuse))
+
+
+
+a  = float(input('Enter first lenght '))  #6
+b  = float(input('Enter second lenght ')) #8
+c = float(input('Enter third lenght '))   #10
+
+trianlge =(c*c == a * a + b * b )
+print(trianlge)
+
